7970005ba914da5b6af4063b370d2e161770ec62
mkslofstra <mariska-karina@hotmail.com>
2019-10-11 13:52:35 +0200

    refactor(GenericDataMapper): Check complete file headers

--- src/main/java/org/molgenis/GenericDataMapper.java
+++ src/main/java/org/molgenis/GenericDataMapper.java
@@ -1,15 +1,22 @@
 package org.molgenis;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
+import javax.validation.UnexpectedTypeException;
 import org.apache.camel.Exchange;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 class GenericDataMapper {
+
   private final AlissaMapper alissaMapper;
   private final LumcMapper lumcMapper;
   private final RadboudMumcMapper radboudMumcMapper;
+
+  private static Log log = LogFactory.getLog(FileCreator.class);
 
   public GenericDataMapper(AlissaMapper alissaMapper, LumcMapper lumcMapper,
       RadboudMumcMapper radboudMumcMapper) {
@@ -18,28 +25,43 @@ class GenericDataMapper {
     this.radboudMumcMapper = radboudMumcMapper;
   }
 
+  private static Boolean isTypeHeader(Set<String> headers, String typeHeader) {
+    return headers.containsAll(Arrays.asList(typeHeader.split("\t")));
+  }
+
   static String getType(Set<String> headers) {
-    if (headers.contains("gDNA_normalized")) {
+    if (isTypeHeader(headers,
+        LumcMapper.LUMC_HEADERS
+            .replace("hgvs_normalized", "gDNA_normalized"))) {
       return "lumc";
-    } else if (headers.contains("empty1")) {
+    } else if (isTypeHeader(headers, RadboudMumcMapper.RADBOUD_HEADERS)) {
       return "radboud";
-    } else {
+    } else if (isTypeHeader(headers,
+        AlissaMapper.ALISSA_HEADERS
+            .replace("_orig", ""))) {
       return "alissa";
+    } else {
+      throw new UnexpectedTypeException(
+          "Lab type not recognized, check headers with headers of alissa, radboud, and lumc");
     }
   }
 
   void mapData(Exchange exchange) {
     Map<String, Object> body = (Map<String, Object>) exchange.getIn().getBody();
     Set<String> headers = body.keySet();
-    String labType = getType(headers);
-    exchange.getIn().getHeaders().put("labType", labType);
+    try {
+      String labType = getType(headers);
+      exchange.getIn().getHeaders().put("labType", labType);
 
-    if (labType.equals("lumc")) {
-      lumcMapper.mapData(body);
-    } else if (labType.equals("radboud")) {
-      radboudMumcMapper.mapData(body);
-    } else {
-      alissaMapper.mapData(body);
+      if (labType.equals("lumc")) {
+        lumcMapper.mapData(body);
+      } else if (labType.equals("radboud")) {
+        radboudMumcMapper.mapData(body);
+      } else {
+        alissaMapper.mapData(body);
+      }
+    } catch (UnexpectedTypeException ex) {
+      log.error(ex);
     }
   }
 }
--- src/test/java/org/molgenis/GenericDataMapperTest.java
+++ src/test/java/org/molgenis/GenericDataMapperTest.java
@@ -1,31 +1,53 @@
 package org.molgenis;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.molgenis.GenericDataMapper.getType;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import javax.validation.UnexpectedTypeException;
 import org.junit.jupiter.api.Test;
 
 class GenericDataMapperTest {
 
   @Test
   void getTypeLumcTest() {
-    Set<String> set = new HashSet<>(Arrays.asList("a", "b", "gDNA_normalized"));
+    Set<String> set = new HashSet<>(Arrays
+        .asList("refseq_build", "chromosome", "gDNA_normalized", "variant_effect", "geneid", "cDNA",
+            "Protein"));
     assertEquals("lumc", getType(set));
   }
 
   @Test
   void getTypeRadboudTest() {
-    Set<String> set = new HashSet<>(Arrays.asList("a", "b", "empty1"));
+    Set<String> set = new HashSet<>(Arrays
+        .asList("chromosome_orig", "start", "stop", "ref_orig", "alt_orig", "gene", "transcript",
+            "protein", "empty1", "exon", "empty2", "classification"));
     assertEquals("radboud", getType(set));
   }
 
   @Test
   void getTypeAlissaTest() {
-    Set<String> set = new HashSet<>(Arrays.asList("a", "b", "c"));
+    Set<String> set = new HashSet<>(Arrays
+        //timestamp	id	chromosome	start	stop	ref	alt	gene	transcript	c_nomen	p_nomen	exon	variant_type	location	effect	classification	last_updated_by	last_updated_on
+        .asList("timestamp", "id", "chromosome", "start", "stop", "ref", "alt", "gene",
+            "transcript", "c_nomen", "p_nomen", "exon", "variant_type", "location", "effect",
+            "classification", "last_updated_by", "last_updated_on"));
     assertEquals("alissa", getType(set));
+  }
+
+  @Test
+  void getTypeErrorTest() {
+    Set<String> set = new HashSet<>(Arrays.asList("my", "beautiful", "shoppinglist"));
+    try {
+      getType(set);
+    } catch (UnexpectedTypeException caughtException) {
+      assertThat(caughtException.getMessage(),
+          is("Lab type not recognized, check headers with headers of alissa, radboud, and lumc"));
+    }
   }
 
 }
