027062f183ae817466b8c44df3808919e8ef7930
mkslofstra <mariska-karina@hotmail.com>
2020-12-14 12:16:00 +0100

    test: assert output files are equal to expected snapshot file

--- src/test/java/org/molgenis/MyFirstCamelTest.java
+++ src/test/java/org/molgenis/MyFirstCamelTest.java
@@ -1,10 +1,9 @@
 package org.molgenis;
 
-import java.io.BufferedReader;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import org.apache.camel.CamelContext;
@@ -40,53 +39,6 @@ public class MyFirstCamelTest {
     return FileUtils.getFile("src", "test", "resources", name);
   }
 
-  private String getHeader(File textFile) throws IOException {
-    BufferedReader br = new BufferedReader(new FileReader(textFile));
-    return br.readLine();
-  }
-
-  private void testHeader(String inputFileName, String outputFileName) throws Exception {
-    File inputFile = getInputFile(inputFileName);
-
-    camelContext.getRouteDefinition("outputFileRoute")
-        .adviceWith(camelContext, new AdviceWithRouteBuilder() {
-          @Override
-          public void configure() {
-            interceptSendToEndpoint("direct:map_data")
-                .skipSendToOriginalEndpoint()
-                .to(MOCK_RESULT);
-          }
-        });
-    camelContext.start();
-    File testInput = new File(
-        "src" + File.separator + "test" + File.separator + "inbox" + File.separator
-            + inputFileName);
-    FileUtils.copyFile(inputFile, testInput);
-    resultEndpoint.setResultWaitTime(20000);
-    resultEndpoint.assertIsSatisfied();
-    resultEndpoint.expectedMessageCount(1);
-    camelContext.stop();
-    String header = getHeader(FileUtils.getFile("result", outputFileName));
-    assert (header.equals(
-        "id\tchromosome\tstart\tstop\tref\talt\tgene\tc_dna\thgvs_g\thgvs_c\ttranscript\tprotein\ttype\tlocation\texon\teffect\tclassification\tcomments\tis_legacy\tlab_upload_date"));
-    testInput.delete();
-  }
-
-  @Test
-  public void testAlissaHeader() throws Exception {
-    testHeader("test_alissa.txt", "vkgl_test_alissa.tsv");
-  }
-
-  @Test
-  public void testRadboudMumcHeader() throws Exception {
-    testHeader("test_radboud_mumc.tsv", "vkgl_test_radboud_mumc.tsv");
-  }
-
-  @Test
-  public void testLumcHeader() throws Exception {
-    testHeader("test_lumc.tsv", "vkgl_test_lumc.tsv");
-  }
-
   private void setMockEndpoint(String mockUri, String route) throws Exception {
     camelContext.getRouteDefinition(route)
         .adviceWith(camelContext, new AdviceWithRouteBuilder() {
@@ -101,9 +53,11 @@ public class MyFirstCamelTest {
       Integer correctVariants, Integer errorVariants) throws Exception {
     File inputFile = getInputFile(inputFileName);
     // Delete old error file if it exists (errors will be added to existing error file)
-    File errorFile = new File("result/vkgl_test_" + lab + "_error.txt");
+    File errorFile = FileUtils.getFile("result", "vkgl_test_" + lab + "_error.txt");
     Files.deleteIfExists(errorFile.toPath());
-    InputStream mockResponse = new FileInputStream("src/test/resources/" + lab + "_mock.json");
+    InputStream mockResponse = new FileInputStream(
+        "src" + File.separator + "test" + File.separator + "resources" + File.separator + lab
+            + "_mock.json");
     MockEndpoint errorEndpoint = camelContext.getEndpoint("mock:error", MockEndpoint.class);
     MockEndpoint labSpecificEndpoint = camelContext.getEndpoint("mock:lab", MockEndpoint.class);
 
@@ -136,6 +90,10 @@ public class MyFirstCamelTest {
     errorEndpoint.assertIsSatisfied();
     labSpecificEndpoint.assertIsSatisfied();
     camelContext.stop();
+    // Check if output files look like expected
+    File snapshot = FileUtils.getFile("src", "test", "resources", "snapshot_" + lab + ".tsv");
+    File actual = FileUtils.getFile("result", "vkgl_test_" + lab + ".tsv");
+    assertTrue(FileUtils.contentEquals(snapshot, actual));
   }
 
   @Test
